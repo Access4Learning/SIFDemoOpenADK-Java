1b61ee118aa3f48d82707529a37d6c092472dfc4
Joerg Huber <joerg.huber@systemic.com.au>
2013-03-15 11:52:02 +0800

    Updated to take new library versions into account.

--- SIFDemoOpenADK/scripts/startAgent.bat
+++ SIFDemoOpenADK/scripts/startAgent.bat
@@ -17,7 +17,10 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 for /f %%a IN ('dir /b /S %LIB_PATH%\*.jar') do set AGENT_CLASS_PATH=!AGENT_CLASS_PATH!;%%a
 
 rem set the config dir and the main executable jar in the classpath
-set AGENT_CLASS_PATH=%AGENT_CLASS_PATH%;%CONFIG_PATH%;%EXE_JAR_PATH%\sifdemo-au_1.2-v1.1.jar
+set AGENT_CLASS_PATH=%AGENT_CLASS_PATH%;%CONFIG_PATH%;%EXE_JAR_PATH%\sifdemo-au_1.3-v1.1.jar
+
+set AGENT_ID=%1%
+set AGENT_PROP_FILE=%2%
 
 echo ======================================================================================================
 echo Start Agent with JVM Settings:
@@ -25,7 +28,8 @@ echo %JVM_SETTINGS%
 echo ======================================================================================================
 echo Start Agent with Classpath:
 echo %AGENT_CLASS_PATH%
-echo Name of Agent: %1%
+echo Name of Agent: %AGENT_ID%
+echo Name of Agent Properties File: %AGENT_PROP_FILE%
 echo ======================================================================================================
 
-%JAVA_HOME%\bin\java %JVM_SETTINGS% -cp %AGENT_CLASS_PATH% systemic.sif.sifcommon.agent.SIFCommonAgent %1%
+%JAVA_HOME%\bin\java %JVM_SETTINGS% -cp %AGENT_CLASS_PATH% systemic.sif.sifcommon.agent.SIFCommonAgent %AGENT_ID% %AGENT_PROP_FILE%
